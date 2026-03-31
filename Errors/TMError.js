2882535a1f4911cf1b4a9dc9ac6967b54c5f319b
Addis Negash <anegash154@gmail.com>
2026-03-31 13:22:50 +0300

    feat : define TMError that extends Error

---
+++ Errors/TMError.js
@@ -0,0 +1,10 @@
+class TMError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.name = this.constructor.name;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = { TMError };
