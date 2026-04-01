1893512d006c33d02f8fccfb4e6ed611a50e0f8f
Addis Negash <anegash154@gmail.com>
2026-04-01 14:51:08 +0300

    feat : define authController function

---
+++ controller/authController.js
@@ -0,0 +1,18 @@
+const {send }=require('../utils/send.js');
+const {login}=require('../service/loginauth.js');
+const {sendError}=require('../utils/sendError.js');
+const {bodyParser}=require('../middleware/bodyParser.js');
+
+async function loginController(req,res){
+    try{
+        const body=await bodyParser(req);
+        const result=await login(body.username,body.password);
+        res.setHeader(`set-cookie`,`sessionId=${result.session}; HttpOnly`);
+        send(res,200,{message:`Login as ${body.username}`});
+    }
+    catch(err){
+       sendError(res,err);
+    }
+}
+
+module.exports={loginController};
