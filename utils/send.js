5f6cc1f7835e71f80d4c9aa08d2ccb5490768506
Addis Negash <anegash154@gmail.com>
2026-04-01 12:55:50 +0300

    feat : define send function

---
+++ utils/send.js
@@ -0,0 +1,7 @@
+function send(res,statusCode,body){
+    res.statusCode=statusCode;
+    res.setHeader('Contnt-Type','application/json');
+    res.end(JSON.stringify(body,null,2));
+    return;
+}
+module.exports={send};
