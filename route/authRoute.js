e32df6d7c639aa8091d9b460e3993791636011c8
Addis Negash <anegash154@gmail.com>
2026-04-04 15:48:23 +0300

    feat : add return statement

--- route/authRoute.js
+++ route/authRoute.js
@@ -4,6 +4,7 @@ async function authRoute(req,res){
     if(req.method==='POST' && req.url==='/login'){
         return await loginController(req,res);
     }
+    return false;
 }
 
 module.exports={authRoute};
