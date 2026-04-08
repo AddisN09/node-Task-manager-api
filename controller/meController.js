91ff2cf82a57848bb6f89fc4d279e97d210e4892
Addis Negash <anegash154@gmail.com>
2026-04-08 19:55:20 +0300

    fix : remove the cookie based authentication from here

--- controller/meController.js
+++ controller/meController.js
@@ -1,24 +1,9 @@
 const {send}=require('../utils/send.js');
-const {sendError}=require('../utils/sendError.js')
-const {cookieParser}=require('../middleware/cookieParser.js');
-const {getSession}=require('../utils/sessionManager.js');
 const { decryptData } = require('../utils/encrypt-decrypt.js');
 
-
-
 async function meController(req,res){
-    const cookie=await cookieParser(req);
-    const sessionId=cookie.sessionId;
-    if(!sessionId){
-         return sendError(res,{message:`Unauthorized no session`,statusCode:500});
-    }
-    const user=await getSession(sessionId);
-    if(!user){
-         return sendError(res,{message:`unauthorized you need to login first`,statusCode:400});
-    }
-    const decryptedId=await decryptData(user.userId.data,user.userId.iv);
-    const decryptedRole=await decryptData(user.role.data,user.role.iv);
+    const decryptedId=await decryptData(req.user.userId.data,req.user.userId.iv);
+    const decryptedRole=await decryptData(req.user.role.data,req.user.role.iv);
     return send(res,200,{userId:decryptedId,role:decryptedRole});
-
 }
 module.exports={meController};
