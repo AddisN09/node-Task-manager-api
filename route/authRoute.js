2c261d5f1beb0a0f4a6fc7e31d3bbbc972733513
Addis Negash <anegash154@gmail.com>
2026-04-10 20:21:28 +0300

    fix : define a route for logout service

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,8 +1,14 @@
 const { loginController } = require("../controller/authController");
+const {requireAuth}=require('../middleware/requireAuth.js');
+const { logoutController } = require("../controller/logoutController");
 
 async function authRoute(req,res){
     if(req.method==='POST' && req.url==='/login'){
         return await loginController(req,res);
+    }
+    if(req.method==='POST' && req.url==='/logout'){
+         if(await requireAuth(req,res))return true;
+         return logoutController(req,res);
     }
     return false;
 }
--- route/meRoute.js
+++ route/meRoute.js
@@ -2,7 +2,7 @@ const {meController}=require('../controller/meController.js');
 const {requireAuth}=require('../middleware/requireAuth.js');
 async function meRoute(req,res){
     if(req.url==='/login/me' && req.method==='GET'){
-        if(!await requireAuth(req,res))return true;
+        if(await requireAuth(req,res))return true;
         return await meController(req,res);
     }
     return false;
