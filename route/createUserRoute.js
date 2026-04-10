2e7d28f314e7d92cce521867922a48976485a850
Addis Negash <anegash154@gmail.com>
2026-04-10 23:12:48 +0300

    feat : implement role based auth to creat user route

--- route/createUserRoute.js
+++ route/createUserRoute.js
@@ -1,7 +1,13 @@
 const {createUserController}=require('../controller/createUserController.js');
+const { requireAuth } = require('../middleware/requireAuth.js');
+const {requireRole}=require('../middleware/requireRole.js');
 
 async function createUserRoute(req,res){
     if(req.url==='/login/user' && req.method==='POST'){
+        if(await requireAuth(req,res))return true;
+
+        if(await requireRole(req,res))return true;
+
         return await createUserController(req,res);
     }
     return false;
