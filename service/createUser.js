c5e000a4b423f29705ff90c4889e85a717fa13ce
Addis Negash <anegash154@gmail.com>
2026-04-11 09:17:53 +0300

    feat : add a condition to avoid multiple users with the same username

--- service/createUser.js
+++ service/createUser.js
@@ -1,10 +1,16 @@
 const path=require('path');
 const {userModel}=require('../models/userModel.js');
 const {readFileContent,writeFileContent}=require('../utils/dataAcces.js');
+const { findUserByUsername } = require('../utils/findUser.js');
+const { TMError } = require('../Errors/TMError.js');
 
 const filePath=path.join(__dirname,'../data/user.json');
 
 async function createUser(username,password,role){
+    const oldUser=await findUserByUsername(username);
+    if(oldUser){
+        throw new TMError(`This user name is occupied`);
+    }
     const newUser= await userModel(username,password,role);
     const users=await readFileContent(filePath);
     users.push(newUser);
