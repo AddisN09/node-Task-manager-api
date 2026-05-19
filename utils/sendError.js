beaf7f61a7063403c9042767633f9f550cc5b6f7
Addis Negash <anegash154@gmail.com>
2026-05-19 12:18:50 +0300

    refactor : correct typo issue

--- utils/sendError.js
+++ utils/sendError.js
@@ -1,6 +1,6 @@
 function sendError(res,error){
-    res.statuCode=error.statusCode;
-    res.end(JSON.stringify({success:false,statusCode:error.statusCode||500,message:error.message||`Internal server error`},null,2));
+    res.statusCode=error.statusCode;
+    res.end(JSON.stringify({success:false,statusCode:res.statusCode||500,message:error.message||`Internal server error`},null,2));
    return true;
 }
 module.exports={sendError};
